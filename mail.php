5b525600462a909646ef512b175b9344de45110d
neelsanja <89695449+neelsanja@users.noreply.github.com>
2021-09-11 00:20:50 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,22 @@
+<?php
+//get data from form  
+
+$name = $_POST['txtName'];
+$email= $_POST['txtEmail'];
+$when= $_POST['app_date'];
+$startd= $_POST['strtDest'];
+$endd= $_POST['endDest'];
+$vehicle= $_POST['selVehicle'];
+$number= $_POST['txtContactNo'];
+
+$to = "neelsanja001@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n When =" . $when;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
